bed209c73d13afa84a92070468071e0b23636663
srikar0896 <srikar.0896@gmail.com>
2018-12-07 23:04:01 +0530

    ssskk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
-import GraphView from "./GraphView";
+import FilterSidebar from './FilterSidebar';
+import GraphView from './GraphView';
+
+const categoriesList = [{
+  value: 'temp',
+  label: 'Temp',
+}];
+
+const subCategoriesList = [{
+  value: 'temp',
+  label: 'Temp',
+}];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedCategory: '',
+      selectedSubCategory: '',
+    };
+
+    this.selectCategory = this.selectCategory.bind(this);
+    this.selectSubCategory = this.selectSubCategory.bind(this);
+    this.applyFilter = this.applyFilter.bind(this);
+  }
+
+  selectCategory(value) {
+    console.log('selectCategory');
+    console.dir({value: value});
+    this.setState({selectedCategory: value});
+  }
+
+  selectSubCategory(value) {
+    console.log('selectSubCategory');
+    console.dir({value: value});
+    this.setState({selectedSubCategory: value});
+  }
+
+  applyFilter(filters) {
+    console.log('applyFilter');
+    console.dir(filters);
+  }
+
   render() {
     return (
       <div className="App">
+        <FilterSidebar
+          selectedCategory={this.state.selectedCategory}
+          selectedSubCategory={this.state.selectedSubCategory}
+          categoriesList={categoriesList}
+          subCategoriesList={subCategoriesList}
+          onCategorySelection={this.selectCategory}
+          onSubCategorySelection={this.selectSubCategory}
+          onFilterApply={this.applyFilter}
+        />
 	      <GraphView />
       </div>
     );
--- src/data/SellerData.js
+++ src/data/SellerData.js
@@ -1,47 +1,232 @@
 export default [
 	{
-		"Minimum": 84,
-		"Average": 82,
-		"Maximum": 68,
+		"id": "japan",
+		"color": "hsl(121, 70%, 50%)",
+		"data": [
+			{
+				"x": "plane",
+				"y": 170
+			},
+			{
+				"x": "helicopter",
+				"y": 247
+			},
+			{
+				"x": "boat",
+				"y": 71
+			},
+			{
+				"x": "train",
+				"y": 229
+			},
+			{
+				"x": "subway",
+				"y": 39
+			},
+			{
+				"x": "bus",
+				"y": 115
+			},
+			{
+				"x": "car",
+				"y": 199
+			},
+			{
+				"x": "moto",
+				"y": 97
+			},
+			{
+				"x": "bicycle",
+				"y": 248
+			},
+			{
+				"x": "others",
+				"y": 43
+			}
+		]
 	},
 	{
-		"Minimum": 143,
-		"Average": 131,
-		"Maximum": 159,
+		"id": "france",
+		"color": "hsl(338, 70%, 50%)",
+		"data": [
+			{
+				"x": "plane",
+				"y": 157
+			},
+			{
+				"x": "helicopter",
+				"y": 112
+			},
+			{
+				"x": "boat",
+				"y": 240
+			},
+			{
+				"x": "train",
+				"y": 66
+			},
+			{
+				"x": "subway",
+				"y": 228
+			},
+			{
+				"x": "bus",
+				"y": 218
+			},
+			{
+				"x": "car",
+				"y": 72
+			},
+			{
+				"x": "moto",
+				"y": 136
+			},
+			{
+				"x": "bicycle",
+				"y": 277
+			},
+			{
+				"x": "others",
+				"y": 294
+			}
+		]
 	},
 	{
-		"Minimum": 196,
-		"Average": 31,
-		"Maximum": 147,
+		"id": "us",
+		"color": "hsl(259, 70%, 50%)",
+		"data": [
+			{
+				"x": "plane",
+				"y": 87
+			},
+			{
+				"x": "helicopter",
+				"y": 183
+			},
+			{
+				"x": "boat",
+				"y": 149
+			},
+			{
+				"x": "train",
+				"y": 86
+			},
+			{
+				"x": "subway",
+				"y": 267
+			},
+			{
+				"x": "bus",
+				"y": 96
+			},
+			{
+				"x": "car",
+				"y": 58
+			},
+			{
+				"x": "moto",
+				"y": 276
+			},
+			{
+				"x": "bicycle",
+				"y": 162
+			},
+			{
+				"x": "others",
+				"y": 245
+			}
+		]
 	},
 	{
-		"Minimum": 59,
-		"Average": 26,
-		"Maximum": 86,
+		"id": "germany",
+		"color": "hsl(182, 70%, 50%)",
+		"data": [
+			{
+				"x": "plane",
+				"y": 200
+			},
+			{
+				"x": "helicopter",
+				"y": 83
+			},
+			{
+				"x": "boat",
+				"y": 143
+			},
+			{
+				"x": "train",
+				"y": 253
+			},
+			{
+				"x": "subway",
+				"y": 133
+			},
+			{
+				"x": "bus",
+				"y": 126
+			},
+			{
+				"x": "car",
+				"y": 294
+			},
+			{
+				"x": "moto",
+				"y": 296
+			},
+			{
+				"x": "bicycle",
+				"y": 175
+			},
+			{
+				"x": "others",
+				"y": 219
+			}
+		]
 	},
 	{
-		"Minimum": 167,
-		"Average": 159,
-		"Maximum": 105,
-	},
-	{
-		"Minimum": 190,
-		"Average": 57,
-		"Maximum": 34,
-	},
-	{
-		"Minimum": 33,
-		"Average": 116,
-		"Maximum": 73,
-	},
-	{
-		"Minimum": 14,
-		"Average": 173,
-		"Maximum": 43,
-	},
-	{
-		"Minimum": 151,
-		"Average": 170,
-		"Maximum": 12,
+		"id": "norway",
+		"color": "hsl(235, 70%, 50%)",
+		"data": [
+			{
+				"x": "plane",
+				"y": 297
+			},
+			{
+				"x": "helicopter",
+				"y": 44
+			},
+			{
+				"x": "boat",
+				"y": 235
+			},
+			{
+				"x": "train",
+				"y": 184
+			},
+			{
+				"x": "subway",
+				"y": 221
+			},
+			{
+				"x": "bus",
+				"y": 2
+			},
+			{
+				"x": "car",
+				"y": 259
+			},
+			{
+				"x": "moto",
+				"y": 264
+			},
+			{
+				"x": "bicycle",
+				"y": 263
+			},
+			{
+				"x": "others",
+				"y": 56
+			}
+		]
 	}
-];
+]
