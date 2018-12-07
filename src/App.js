9f52afb3fef3e7f6eee4f83ebc27e696513945be
Sameer Kumar <sameer.kumar@bizongo.in>
2018-12-07 23:00:23 +0530

    Added real mock data

--- src/App.js
+++ src/App.js
@@ -12,16 +12,19 @@ class App extends Component {
     this.state = {
       selectedCategory: '',
       selectedSubCategory: '',
-      product: '',
+      selectedProduct: '',
+      selectedDimension: '',
     };
 
     this.selectCategory = this.selectCategory.bind(this);
     this.selectSubCategory = this.selectSubCategory.bind(this);
     this.selectProduct = this.selectProduct.bind(this);
+    this.selectDimension = this.selectDimension.bind(this);
     this.applyFilter = this.applyFilter.bind(this);
     this.getSubCategories = this.getSubCategories.bind(this);
     this.getProducts = this.getProducts.bind(this);
     this.getDimensions = this.getDimensions.bind(this);
+    this.getSpecs = this.getSpecs.bind(this);
   }
 
   getSubCategories() {
@@ -45,11 +48,25 @@ class App extends Component {
   getDimensions() {
     console.log('getDimensions');
     console.dir({State: this.state});
-    if (this.state.selectedCategory && this.state.selectedSubCategory && this.state.product) {
+    if (this.state.selectedCategory && this.state.selectedSubCategory && this.state.selectedProduct) {
       return database.getDimensionsList(
         this.state.selectedCategory,
         this.state.selectedSubCategory,
-        this.state.product
+        this.state.selectedProduct
+      )
+    }
+    else return [];
+  }
+
+  getSpecs() {
+    console.log('getSpecsList');
+    console.dir({State: this.state});
+    if (this.state.selectedCategory && this.state.selectedSubCategory && this.state.selectedProduct && this.state.selectedDimension) {
+      return database.getSpecsList(
+        this.state.selectedCategory,
+        this.state.selectedSubCategory,
+        this.state.selectedProduct,
+        this.state.selectedDimension,
       )
     }
     else return [];
@@ -70,7 +87,13 @@ class App extends Component {
   selectProduct(value) {
     console.log('selectProduct');
     console.dir({value: value});
-    this.setState({product: value});
+    this.setState({selectedProduct: value});
+  }
+
+  selectDimension(value) {
+    console.log('selectDimension');
+    console.dir({value: value});
+    this.setState({selectedDimension: value});
   }
 
   applyFilter(filters) {
@@ -84,14 +107,17 @@ class App extends Component {
         <FilterSidebar
           selectedCategory={this.state.selectedCategory}
           selectedSubCategory={this.state.selectedSubCategory}
-          selectedProduct={this.state.product}
+          selectedProduct={this.state.selectedProduct}
+          selectedDimension={this.state.selectedDimension}
           categoriesList={database.getCategoriesList()}
           subCategoriesList={this.getSubCategories()}
           productsList={this.getProducts()}
           dimensionsList={this.getDimensions()}
+          specsList={this.getSpecs()}
           onCategorySelection={this.selectCategory}
           onSubCategorySelection={this.selectSubCategory}
           onProductSelection={this.selectProduct}
+          onDimensionSelection={this.selectDimension}
           onFilterApply={this.applyFilter}
         />
       </div>
